6b2c057db3c91331aa6b7bf71835a393a4aa8cf3
Rashi <rashighosh96@gmail.com>
2019-07-20 16:53:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,9 @@ else {
     document.body.style.backgroundImage = "url('night.svg')";
 }
 
+window.addEventListener('resize', () => {
+    // We execute the same script as before
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  });
+
