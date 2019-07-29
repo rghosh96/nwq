6f1ad1f5a217038fe353abc2951f7f1952ed95dc
Rashi Ghosh <rghosh@Rashis-MBP.kc.umkc.edu>
2019-07-28 21:14:39 -0500

    added modal for gatewood

--- script.js
+++ script.js
@@ -14,3 +14,27 @@ else {
     document.body.style.backgroundImage = "url('night.svg')";
 }
 
+//duty button
+var g = document.getElementById("g");
+var h = document.getElementById("h");
+var m = document.getElementById("m");
+var c = document.getElementById("c");
+var modal = document.getElementById("modal");
+var building = document.getElementById("gatewood");
+var exit = document.getElementsByClassName("close")[0];
+g.onclick = function() {
+    modal.style.display = "block";
+    g.style.visibility = "hidden";
+    h.style.visibility = "hidden";
+    m.style.visibility = "hidden";
+    c.style.visibility = "hidden";
+    building.style.transform = "scale(1.5)";
+}
+exit.onclick = function() {
+    modal.style.display = "none";
+    g.style.visibility = "visible";
+    h.style.visibility = "visible";
+    m.style.visibility = "visible";
+    c.style.visibility = "visible";
+    building.style.transform = "scale(1)";
+}
